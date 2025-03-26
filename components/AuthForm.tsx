b137f9a5e97428a1880dd1c863db8135ef7966df
babylink1 <babylinkbabylink@gmail.com>
2025-03-26 18:08:44 -0400

    fix: implement sign-in flow with ID token and toast feedback

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,7 +17,8 @@ import { useRouter } from "next/navigation";
 import FormField from "@/components/FormField";
 import {createUserWithEmailAndPassword} from "@firebase/auth";
 import {auth} from "@/firebase/client";
-import {signUp} from "@/lib/actions/auth.action";
+import {signIn, signUp} from "@/lib/actions/auth.action";
+import {signInWithEmailAndPassword} from "@firebase/auth";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -69,6 +70,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
+           const {email,password} =values;
+
+           const userCredential =await signInWithEmailAndPassword(auth,email,password);
+
+           const idToken =await userCredential.user.getIdToken();
+
+           if(!idToken){
+             toast.error('Sign in failed')
+             return;
+           }
+          await signIn({
+            email,idToken
+
+          })
+
         toast.success("Sign in successfully.");
         router.push("/");
       }
