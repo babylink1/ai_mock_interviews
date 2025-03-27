2789636dd4ea3fab312b75ae29bb43d0ae683609
babylink1 <babylinkbabylink@gmail.com>
2025-03-27 06:53:53 -0400

    fix: comment out unused variables and constants

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,90 +1,93 @@
-
-import React from 'react';
-import Agent from "@/components/Agent";
-import Image from ".next/image";
-
-enum CallStatus {
-    INACTIVE='INACTIVE',
-    CONNECTED='CONNECTED',
-    ACTIVE='ACTIVE',
-    FINISHED='FINISHED',
-
-}
-
-
-
-
-const Page =({userName}AgentProps) =>{
-
-
-    const callStatus=callStatus.FINISHED;
-
-    const isSpeaking =true;
-
-    const messages=[
-      'Whats your name?',
-        'My name is Lecea, nice to meet you!'
-
-    ];
-    const lastMessage= messages[messages.length-1];
-
-
-    return (
-
-        <>
-
-
-            <div className="call-view">
-           <div className="card-interviewer">
-               <div className="avatar">
-                   <Image src="/ai-avatar.png"  alt="vapi" width={65} height={54} className="object-cover"/>
-                   {isSpeaking && <span className ="animate-speakik"/>}
-               </div>
-                <h3>AI Interviewer</h3>
-
-           </div>
-           <div className="card-border">
-             <div className="card-content">
-                 <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px] "/>
-                 <h3>{userName}</h3>
-             </div>
-           </div>
-
-          </div>
-
-            {messages.length>0 && (
-                <div className={"transcript-border">
-                <div  className ="transcript">
-                    <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn-opacity-100')}>
-                {lastMessage}
-                    </p>
-                    </div>
-                    </div>
-                }
-            )}
-
-
-
-            <div className="w-full flex justify-center">
-
-                {callStatus ==='ACTIVE'?(
-                    <button  className="relative btn-call">
-                        <span  className={cn('absolute animate-ping| rounded-full opacity-75',callStatus ==='CONNECTING' & 'hidden')}
-                            />
-
-                            <span>{callStatus==='INACTIVE'|| callStatus ==='FINISHED'? 'Call':'...'}</span>
-
-                        </span>
-                    </button>
-                ):(
-                    <button className="btn-disconnect">
-                        End
-
-                    </button>
-                )}
-
-            </div>
-            </>
-    )
-}
-export default Page;
+//
+// import React from 'react';
+// import Agent from "@/components/Agent";
+// import Image from "next/image";
+//
+// import { cn } from "@/lib/utils";
+//
+//
+// enum CallStatus {
+//     INACTIVE='INACTIVE',
+//     CONNECTED='CONNECTED',
+//     ACTIVE='ACTIVE',
+//     FINISHED='FINISHED',
+//
+// }
+//
+//
+//
+//
+// const Page =({userName}AgentProps) =>{
+//
+//
+//     const callStatus=callStatus.FINISHED;
+//
+//     const isSpeaking =true;
+//
+//     const messages=[
+//       'Whats your name?',
+//         'My name is Lecea, nice to meet you!'
+//
+//     ];
+//     const lastMessage= messages[messages.length-1];
+//
+//
+//     return (
+//
+//         <>
+//
+//
+//             <div className="call-view">
+//            <div className="card-interviewer">
+//                <div className="avatar">
+//                    <Image src="/ai-avatar.png"  alt="vapi" width={65} height={54} className="object-cover"/>
+//                    {isSpeaking && <span className ="animate-speakik"/>}
+//                </div>
+//                 <h3>AI Interviewer</h3>
+//
+//            </div>
+//            <div className="card-border">
+//              <div className="card-content">
+//                  <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px] "/>
+//                  <h3>{userName}</h3>
+//              </div>
+//            </div>
+//
+//           </div>
+//
+//             {messages.length>0 && (
+//                 <div className={"transcript-border">
+//                 <div  className ="transcript">
+//                     <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn-opacity-100')}>
+//                 {lastMessage}
+//                     </p>
+//                     </div>
+//                     </div>
+//                 }
+//             )}
+//
+//
+//
+//             <div className="w-full flex justify-center">
+//
+//                 {callStatus ==='ACTIVE'?(
+//                     <button  className="relative btn-call">
+//                         <span  className={cn('absolute animate-ping| rounded-full opacity-75',callStatus ==='CONNECTING' & 'hidden')}
+//                             />
+//
+//                             <span>{callStatus==='INACTIVE'|| callStatus ==='FINISHED'? 'Call':'...'}</span>
+//
+//                         </span>
+//                     </button>
+//                 ):(
+//                     <button className="btn-disconnect">
+//                         End
+//
+//                     </button>
+//                 )}
+//
+//             </div>
+//             </>
+//     )
+// }
+// export default Page;
